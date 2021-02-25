a2ba4242c0a2646fabaa41ef6080acc89cb945e1
nicogulo <nicogulobelas@gmail.com>
2021-02-25 21:31:40 +0700

    arrow function

--- Object/arrowfunc.js
+++ Object/arrowfunc.js
@@ -3,7 +3,23 @@
 // }
 // console.log(tampilNama('Nico'));
 
-const tampilNama = (nama,waktu) => {
-  return `Selamat ${waktu}, ${nama}`
-}
-console.log(tampilNama('Nico', 'Malam'));
+// const tampilNama = (nama,waktu) => {
+//   return `Selamat ${waktu}, ${nama}`
+// }
+// console.log(tampilNama('Nico', 'Malam'));
+
+// * Implisit return
+
+// const tampilNama = (nama,waktu) => `Selamat ${waktu}, ${nama}`;
+// console.log(tampilNama('Nico', 'Malam'));
+
+
+let mahasiswa = ['Nico', 'Alfian', 'Renaldy Gulo'];
+
+// !tanpa object
+// let jumlahHuruf = mahasiswa.map(nama => nama.length);  
+// console.log(jumlahHuruf);
+
+// ! menggunakan object
+let jumlahHuruf = mahasiswa.map(nama => ({ nama: nama, jmlHuruf: nama.length}));
+console.table(jumlahHuruf);
