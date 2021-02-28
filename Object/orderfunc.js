7e3f949ff3630343565afcade2f552058a133b59
nicogulo <nicogulobelas@gmail.com>
2021-02-28 20:52:08 +0700

    added higher order function

---
+++ Object/orderfunc.js
@@ -0,0 +1,13 @@
+function kerjakanTugas(matakuliah, selesai){
+  console.log(`Mulai Mengerjakan tugas ${matakuliah}...`);
+  selesai();
+}
+
+function selesai(){
+  // setInterval( function(){
+  //   ('Selesai mengerjakan tugas !');
+  // }, 500);
+  // return ;
+  alert('Selesai mengerjakan tugas !');
+}
+kerjakanTugas('Pemrograman Web', selesai);
