eb2326e2b427c6a2b5182688fbf8f19ff2521c9c
nicogulo <nicogulobelas@gmail.com>
2021-02-28 21:41:36 +0700

    Filter

--- Object/dist/orderfunc.dev.js
+++ Object/dist/orderfunc.dev.js
@@ -1,16 +1,37 @@
 "use strict";
 
-function kerjakanTugas(matakuliah, selesai) {
-  console.log("Mulai Mengerjakan tugas ".concat(matakuliah, "..."));
-  selesai();
-}
+// function kerjakanTugas(matakuliah, selesai){
+//   console.log(`Mulai Mengerjakan tugas ${matakuliah}...`);
+//   selesai();
+// }
+// function selesai(){
+//   // setInterval( function(){
+//   //   ('Selesai mengerjakan tugas !');
+//   // }, 500);
+//   // return ;
+//   alert('Selesai mengerjakan tugas !');
+// }
+// kerjakanTugas('Pemrograman Web', selesai); 
+// let total = 0, count = 1;
+// while(count <= 10){
+//   total += count;
+//   count += 1;
+// }
+// console.log(total);
 
-function selesai() {
-  // setInterval( function(){
-  //   ('Selesai mengerjakan tugas !');
-  // }, 500);
-  // return ;
-  alert('Selesai mengerjakan tugas !');
-}
+/* Filter */
+var angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9]; // mencari angka >3
+// For
+// const newAngka = [];
+// for(let i = 0; i < angka.length; i++){
+//   if(angka[i]>=3){
+//     newAngka.push(angka[i]);
+//   }
+// }
+// console.log(newAngka);
+// Filter
 
-kerjakanTugas('Pemrograman Web', selesai);
+var newAngka = angka.filter(function (a) {
+  return a >= 3;
+});
+console.log(newAngka);
--- Object/orderfunc.js
+++ Object/orderfunc.js
@@ -1,13 +1,38 @@
-function kerjakanTugas(matakuliah, selesai){
-  console.log(`Mulai Mengerjakan tugas ${matakuliah}...`);
-  selesai();
-}
+// function kerjakanTugas(matakuliah, selesai){
+//   console.log(`Mulai Mengerjakan tugas ${matakuliah}...`);
+//   selesai();
+// }
 
-function selesai(){
-  // setInterval( function(){
-  //   ('Selesai mengerjakan tugas !');
-  // }, 500);
-  // return ;
-  alert('Selesai mengerjakan tugas !');
-}
-kerjakanTugas('Pemrograman Web', selesai);
+// function selesai(){
+//   // setInterval( function(){
+//   //   ('Selesai mengerjakan tugas !');
+//   // }, 500);
+//   // return ;
+//   alert('Selesai mengerjakan tugas !');
+// }
+// kerjakanTugas('Pemrograman Web', selesai); 
+
+// let total = 0, count = 1;
+// while(count <= 10){
+//   total += count;
+//   count += 1;
+// }
+// console.log(total);
+
+/* Filter */
+const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
+// mencari angka >3
+
+
+// For
+// const newAngka = [];
+// for(let i = 0; i < angka.length; i++){
+//   if(angka[i]>=3){
+//     newAngka.push(angka[i]);
+//   }
+// }
+// console.log(newAngka);
+
+// Filter
+const newAngka = angka.filter((a) => a >= 3 );
+console.log(newAngka);
