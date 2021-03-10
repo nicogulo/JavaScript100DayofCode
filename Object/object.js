5be7d9ad86cb95fc2d45f3b5a5f3d9c48206c10b
nicogulo <nicogulobelas@gmail.com>
2021-03-10 19:48:13 +0700

    added nina

--- Object/object.js
+++ Object/object.js
@@ -4,41 +4,41 @@
 // ! Properti: berisi nilai
 // ! method : berisi fution 
 
-// todo Objecy literal
+// todo Object literal
 // !tidak efektif untuk objeck yang banyak
-// let  mahasiswa = {
+// let breakfast = {
 //   nama: 'Nico',
 //   energi: 10,
 //   makan: function (porsi) {
 //     this.energi =  this.energi + porsi;
-//     return this.energi; 
+//     console.log(`Haloo ${this.nama}, selamat makan. Energi anda sebesaar ${this.energi}`);
 //   }
 // }
 
 
 // todo FUnction Declaration
 // !
-// function Mahasiswa(nama, energi) {
+// function Breakfast(nama, energi) {
 
 // // properti
-//  let mahasiswa = {};
-//  mahasiswa.nama = nama;
-//  mahasiswa.energi = energi;
+//  let people = {};
+//  people.nama = nama;
+//  people.energi = energi;
 
 // //  method
-//  mahasiswa.makan = function (porsi) {
+//  people.makan = function (porsi) {
 //    this.energi += porsi;
 //    console.log(`Haloo ${this.nama}, selamat makan. Energi anda sebesaar ${this.energi}`);
 //  }
-//  mahasiswa.main = function (jam) {
+//  people.main = function (jam) {
 //    this.energi -= jam;
 //    console.log(`Hallo ${this.nama}, selamat bermain`);
 //  }
-//  return mahasiswa;
+//  return people;
 // }
 
-// let nico = Mahasiswa('Nico', 10);
-// let gulo = Mahasiswa('Gulo',20);
+// let nico = Breakfast('Nico', 10);
+// let gulo = Breakfast('Gulo',20);
 
 
 
@@ -83,7 +83,6 @@
 // Mahasiswa.prototype.main = function (jam) {
 //   this.energi -= jam;
 //   return `Hallo ${this.nama}, Anda sedang main, Energi anda sekrang ${this.energi}`;
-
 // }
 
 // Mahasiswa.prototype.tidur = function (jam) {
@@ -95,32 +94,32 @@
 // let Gulo = new Mahasiswa('Gulo', 12);
 
 
-// todo versi class
-class Mahasiswa {
-  constructor(nama, energi) {
-    this.nama = nama;
-    this.energi = energi;
-  }
+ // todo versi class
+// class Mahasiswa {
+//   constructor(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+//   }
 
-  makan = function (porsi) {
-    this.energi += porsi;
-    return `Hallo ${this.nama}, Selamat makan, Energi anda sekrang ${this.energi}`;
-  }
+//   makan = function (porsi) {
+//     this.energi += porsi;
+//     return `Hallo ${this.nama}, Selamat makan, Energi anda sekrang ${this.energi}`;
+//   }
 
-  main = function (jam) {
-    this.energi -= jam;
-    return `Hallo ${this.nama}, Anda sedang main, Energi anda sekrang ${this.energi}`;
+//   main = function (jam) {
+//     this.energi -= jam;
+//     return `Hallo ${this.nama}, Anda sedang main, Energi anda sekrang ${this.energi}`;
 
-  }
+//   }
 
-  tidur = function (jam) {
-    this.energi += jam * 2;
-    return `Hallo ${this.nama}, Selamat tidur, Energi anda akan bertambah  menjadi ${this.energi}`;
+//   tidur = function (jam) {
+//     this.energi += jam * 2;
+//     return `Hallo ${this.nama}, Selamat tidur, Energi anda akan bertambah  menjadi ${this.energi}`;
 
-  }
-}
+//   }
+// }
 
-let nico  = new Mahasiswa('Nico', 10);
+// let nico  = new Mahasiswa('Nico', 10);
 
 
 // todo Object.create
