63cbe564875d93b0fca1398746b02e0299becef5
nicogulo <nicogulobelas@gmail.com>
2021-03-12 16:07:15 +0700

    learn template literal

---
+++ Object/literal.js
@@ -0,0 +1,26 @@
+// Template literal / Template String
+
+// const nama = 'Nico';
+// const umur = 22;
+// console.log(`Hallo nama saya ${nama}, umur saya ${umur}`);
+
+// Embedded Expression
+console.log(`${1 + 1}`);
+
+
+// HTML Fragment
+// const mhs = {
+//     nama: 'Nico',
+//     umur: 22,
+//     nim: 2021731333,
+//     email: 'nico1731333@irpln.ac.id'
+// };
+
+// const el = `<div class="mhs">
+//     <h2>${mhs.nama}</h2>
+//     <span class="nim">${mhs.nim}</span>
+// </div>`;
+
+// console.log(el);
+
+
