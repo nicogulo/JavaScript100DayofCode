54a35803f773508dede2c566c17ddb192b0d5c85
nicogulo <nicogulobelas@gmail.com>
2021-03-18 13:22:47 +0700

    added  function tips

---
+++ Object/function.js
@@ -0,0 +1,22 @@
+// function iniFunction(){
+//   // membuat greeting...
+//   const greeting = sayHalo;
+//   // membuat iniFunction lain
+//   return{
+//     greeting,
+//     iniFunctionLain
+//   }
+// }
+
+ function createGreeting(){
+  //  construct greeting
+   return someGreeting;
+ }
+ function iniFunction(){
+   const greeting = createGreeting();
+  //  create the itu function
+   return {
+     greeting,
+     ituFunction
+   }
+ }
