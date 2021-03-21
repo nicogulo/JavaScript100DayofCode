f2648711f3b2b4b6ebd025cc60661882403386c5
nicogulo <nicogulobelas@gmail.com>
2021-03-21 11:18:35 +0700

    implement Template Literal in Looping, condiitional, nesed

--- Object/literal.js
+++ Object/literal.js
@@ -27,16 +27,85 @@
 // ! HTML  Fragment
 
 
+// const mhs = {
+//     nama: 'Nico',
+//     umur: 22,
+//     nim: '201731333',
+//     email: 'nicogulobelas@gmail.com'
+// };
+
+// const el = `<div class="mhs">
+//     <h2>${mhs.nama}</h2>
+//     <span class="nim">${mhs.nim}</span>
+// </div>`;
+
+
+// ! Looping
+// const mhs= [
+//     {
+//         nama: 'nico',
+//         email: 'nico@gmail.com'
+//     },
+//     {
+//         nama: 'alfiain',
+//         email: 'alfian@gmail.com'
+//     },
+//     {
+//         nama: 'gulo',
+//         email: 'gulo@gmail.com'
+//     }
+// ];
+
+// const el = `<div class="mhs">
+//     ${mhs.map(m => `<ul>
+//         <li>${m.nama}</li>
+//         <li>${m.email}</li>
+//     </ul>` ).join('')}
+// </div>`;
+
+// document.body.innerHTML = el;
+
+
+
+// ! Conditional
+// const music  = {
+//     tittle: 'You',
+//     singer: 'Mahalu',
+//     feat: 'Maeda'
+// }
+
+// const el = `<div class="music">
+//     <ul>
+//         <li>${music.singer}</li>
+//         <li>${music.tittle} ${music.feat ? `(feat. ${music.feat})` : ''}</li>
+//     </ul>
+// </div>`
+
+// document.body.innerHTML = el;
+
+// ! Nested
+
 const mhs = {
-    nama: 'Nico',
-    umur: 22,
-    nim: '201731333',
-    email: 'nicogulobelas@gmail.com'
+    nama: 'nico',
+    semester: 5,
+    matakuliah: ['RPL', 'WEB', 'KALKULUS', 'PBO']
 };
+
+function cetakMatakuliah (matakuliah) {
+    return `
+    <ol>
+        ${matakuliah.map(mk => `<li>${mk}</li>`).join('' )}
+    </ol>
+    `
+}
 
 const el = `<div class="mhs">
     <h2>${mhs.nama}</h2>
-    <span class="nim">${mhs.nim}</span>
+    <span class="semester">Semester ${mhs.semester}</span>
+    <h4>Matakuliah: </h4>
+    ${cetakMatakuliah(mhs.matakuliah)}
 </div>`;
+
+
 
 document.body.innerHTML = el;
