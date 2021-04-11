8ff8436ca0cd1d578fbafb24c0e6a37da72ff697
nicogulo <nicogulobelas@gmail.com>
2021-04-11 19:05:10 +0700

    learn destructuring

---
+++ Object/distructor.js
@@ -0,0 +1,26 @@
+// Destructuring var
+// Destructuring Array
+
+const greeting = ['halo', 'nama', 'saya', 'nico'];
+// ! general
+// const greet = greeting[0];
+// const name = greeting[1];
+// console.log(name);
+
+//  ! use destructuring
+const [greet, one, two, three] = greeting;
+console.log(greet);
+
+
+// Destructuring Object
+// const mhs= {
+//   nama: 'Nico',
+//   umur: 22,
+//   email: 'nicogulobelas@gmail.com'
+// };
+
+
+// const {nama, umur, email} = mhs;
+// console.log(nama);
+// console.log(umur);
+// console.log(email);
