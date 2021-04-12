6e8045f0334f584d437d687cf35e535015d412d5
nicogulo <nicogulobelas@gmail.com>
2021-04-12 09:23:14 +0700

    newlooping

--- Object/distructor.js
+++ Object/distructor.js
@@ -1,15 +1,15 @@
 // Destructuring var
 // Destructuring Array
 
-const greeting = ['halo', 'nama', 'saya', 'nico'];
-// ! general
-// const greet = greeting[0];
-// const name = greeting[1];
-// console.log(name);
+// const greeting = ['halo', 'nama', 'saya', 'nico'];
+// // ! general
+// // const greet = greeting[0];
+// // const name = greeting[1];
+// // console.log(name);
 
-//  ! use destructuring
-const [greet, one, two, three] = greeting;
-console.log(greet);
+// //  ! use destructuring
+// const [greet, one, two, three] = greeting;
+// console.log(greet);
 
 
 // Destructuring Object
@@ -24,3 +24,29 @@ console.log(greet);
 // console.log(nama);
 // console.log(umur);
 // console.log(email);
+
+
+// ! tanpa deklarasi object
+
+
+// ( {nama, umur, email} = {
+//   nama: 'Nico',
+//   umur: 22,
+//   email: 'nicogulobelas@gmail.com'
+// });
+// console.log(nama);
+
+// * 
+
+function math(a, b){
+  return [a + b, a * b];
+}
+
+// general
+// const values = math(2,3)[0];
+// console.log(values);
+
+
+// with destructuring
+const [plus, multiple] = math(2,3);
+console.log(multiple);   
---
+++ Object/newlooping.js
@@ -0,0 +1,29 @@
+// * For .. of array
+
+// const mhs = ['Nico', 'Alfian', 'Renaldy'];
+
+// for(let i = 0; i < mhs.length; i++){
+//   console.log(mhs[i]);
+// }
+
+// mhs.forEach(m => console.log(m));
+
+// for (const m of mhs){
+//   console.log(m);
+// }
+
+
+// *  string
+
+// const nama = 'Nico';
+
+
+// * nodelist
+
+const liNama = document.querySelectorAll('.nama');
+
+// liNama.forEach(n => console.log(n.innerHTML));
+
+for (n of liNama){
+  console.log(n.innerHTML);
+}
