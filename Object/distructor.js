a3bca8c9bda398bfeb4a953b89ecfff8637a3415
nicogulo <nicogulobelas@gmail.com>
2021-04-25 16:52:00 +0700

    try destructuring object Es6

--- Object/distructor.js
+++ Object/distructor.js
@@ -1,52 +1,61 @@
 // Destructuring var
 // Destructuring Array
 
-// const greeting = ['halo', 'nama', 'saya', 'nico'];
-// // ! general
-// // const greet = greeting[0];
-// // const name = greeting[1];
-// // console.log(name);
+// const opp = ["halo", "I am", "a", "Frondend Developer"];
 
-// //  ! use destructuring
-// const [greet, one, two, three] = greeting;
-// console.log(greet);
+// ! general
 
+// const greet = opp[0];
+// const job = opp[3];
+// console.log(job); // Frondend Developer
+
+//  ! use destructuring
+// const [one, two, three, job] = opp;
+// console.log(job); // Frondend Developer
 
 // Destructuring Object
-// const mhs= {
-//   nama: 'Nico',
+// const mhs = {
+//   nama: "Nico",
 //   umur: 22,
-//   email: 'nicogulobelas@gmail.com'
+//   email: "nicogulobelas@gmail.com",
 // };
 
-
-// const {nama, umur, email} = mhs;
+// const { nama, umur, email } = mhs;
 // console.log(nama);
 // console.log(umur);
 // console.log(email);
 
+// ! ES6
+const profile = {
+  firstName: "Dev",
+  lastName: "Nic",
+  age: 22,
+};
+
+const { firstName, lastName, age } = profile;
+
+console.log(firstName, lastName, age);
 
 // ! tanpa deklarasi object
 
-
-// ( {nama, umur, email} = {
-//   nama: 'Nico',
+// ({ nama, umur, email } = {
+//   nama: "Nico",
 //   umur: 22,
-//   email: 'nicogulobelas@gmail.com'
+//   email: "nicogulobelas@gmail.com",
 // });
 // console.log(nama);
+// console.log(umur);
 
-// * 
+// *
 
-function math(a, b){
-  return [a + b, a * b];
-}
+// function math(a, b) {
+//   return [a + b, a * b];
+// }
 
 // general
 // const values = math(2,3)[0];
 // console.log(values);
 
-
 // with destructuring
-const [plus, multiple] = math(2,3);
-console.log(multiple);   
+// const [plus, multiple] = math(2, 3);
+// console.log(multiple);
