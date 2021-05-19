3c81f84e066bb51f68ac8dd6489fe2b9739298d2
nicogulo <nicogulobelas@gmail.com>
2021-05-19 23:30:04 +0700

    nodemon

--- expressbasic/app.js
+++ expressbasic/app.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello Express!');
 });
-app.get('/');
+app.get('/:name', (req, res) => res.send(`${req.params.name} here`));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
