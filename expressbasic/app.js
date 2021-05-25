eabb2d920cf6bb364a46a7a39e0161231dfcf011
nicogulo <nicogulobelas@gmail.com>
2021-05-26 01:11:22 +0700

    parsing data and render

--- expressbasic/app.js
+++ expressbasic/app.js
@@ -6,9 +6,11 @@ app.set('views', './views'); // specify the views directory
 app.set('view engine', 'ejs'); // register the template engine
 
 app.get('/', (req, res) => {
+  const fruit = [{ name: 'appel' }, { name: 'watermelon' }, { name: 'mango' }];
   res.render('index', {
     name: 'Maeda',
     greet: 'good days',
+    fruit: fruit,
   });
 });
 app.get('/:name', (req, res) => res.send(`${req.params.name} here`));
